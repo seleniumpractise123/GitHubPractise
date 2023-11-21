579d5a6a14ffc30b6da388acf53d70dae5673c83
Naveen Sirivella <naveensalesforce.learning70@gmail.com>
2023-11-21 07:39:33 +0530

    added cart page class

---
+++ src/Pages/CartPage.java
@@ -0,0 +1,12 @@
+package Pages;
+
+public class CartPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is Cart page");
+
+	}
+
+}
