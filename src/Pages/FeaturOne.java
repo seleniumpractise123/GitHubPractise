99bef9412537774cc2691d58f39839ee7cf567a3
Naveen Sirivella <naveensalesforce.learning70@gmail.com>
2023-11-22 11:39:22 +0530

    added new class

---
+++ src/Pages/FeaturOne.java
@@ -0,0 +1,15 @@
+package Pages;
+
+public class FeaturOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("f1");
+		System.out.println("f2");
+
+		System.out.println("f3");
+		System.out.println("f4");
+	}
+
+}
